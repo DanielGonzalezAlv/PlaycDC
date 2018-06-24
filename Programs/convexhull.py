653516011e2bf4abe0d22e6277ee87535e4c86a8
Daniel <d.gonzalez@stud.uni-heidelberg.de>
2018-06-24 21:40:54 +0200

    new

--- Programs/convexhull.py
+++ Programs/convexhull.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     # Insert your Path and image
     
     path_img = "../Data/Images/Post-Processed/"
-    cardname = "2s_2"
+    cardname = "qs_2"
     ###########################
     
     img = cv2.imread(path_img + cardname +".JPG")
@@ -58,13 +58,13 @@ if __name__ == "__main__":
     height, width, _ = img.shape
             
     # For the first bounding box
-    xmin1, xmax1 = 4, 100   
-    ymin1, ymax1 = 4, 230
+    xmin1, xmax1 = 2, 73  
+    ymin1, ymax1 = 2, 230
     img1 = img[ymin1:ymax1, xmin1:xmax1, :]
     
     # For the second bounding box
-    xmin2, xmax2 = -100, -1     # This values have to be negative
-    ymin2, ymax2 = -220, -1     # This values have to be negative
+    xmin2, xmax2 = -79, -1     # This values have to be negative
+    ymin2, ymax2 = -250, -1     # This values have to be negative
     img2 = img[ymin2:ymax2, xmin2:, :]
     
     # compute convex Hulls 
