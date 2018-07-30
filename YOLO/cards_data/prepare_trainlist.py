bb3179d199df6948fde217134341f9cdfdad073f
frank <kpazrael2@gmail.com>
2018-07-31 00:14:05 +0200

    done poster

--- YOLO/cards_data/prepare_trainlist.py
+++ YOLO/cards_data/prepare_trainlist.py
@@ -1,8 +1,13 @@
 import os
-folder = 'JPEGImages'
+folder = 'cards_data/JPEGImages'
 path="./" + folder  
 dirList=os.listdir(path)
-with open("cardstrain.txt", "w") as f:
-    for filename in dirList:
+with open("cards_data/cardstrain.txt", "w") as f:
+    # create train images
+    for filename in dirList[:-2000]:
+        f.write(os.path.abspath(os.path.join(folder, filename))+'\n') 
+with open("cards_data/cardsval.txt", "w") as f:
+    # create test images
+    for filename in dirList[-2000:]:
         f.write(os.path.abspath(os.path.join(folder, filename))+'\n') 
 
--- YOLO/cards_data/remove_empty.py
+++
@@ -1,8 +0,0 @@
-import os
-for file in os.listdir('./labels'):
-    with open('./labels/' + file, 'r') as f:
-        lines = f.readlines()
-        print(len(lines))
-        if len(lines) == 0:
-            os.remove('./labels/' + file)
-            os.remove('./JPEGImages/' + file[:-4] + '.jpg')
